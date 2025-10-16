3b975d1ebf532276ec1fbc96df9e4a0c97f8304c
Owen Petersen <opetersen1010@hotmail.com>
2025-10-16 01:46:35 -0400

    Print more text

--- src/main/java/net/azurewebsites/amazin_online_bookstore/main.java
+++ src/main/java/net/azurewebsites/amazin_online_bookstore/main.java
@@ -2,6 +2,6 @@ package net.azurewebsites.amazin_online_bookstore;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World, and Hello SYSC 4806");
     }
 }
