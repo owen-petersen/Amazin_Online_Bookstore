17e78160a929f706c0fdc351f7b4360dbd415680
Faareh <faarehb@yahoo.com>
2025-11-02 10:51:44 -0500

    Doc: Add TODOs for future updates in HomeController

--- src/main/java/net/azurewebsites/amazin_online_bookstore/HomeController.java
+++ src/main/java/net/azurewebsites/amazin_online_bookstore/HomeController.java
@@ -8,11 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    // TODO: Redirect here after login once login flow is updated.
+    // (home page) instead of welcome.html.
+
     @Autowired
     private BookRepository bookRepository;
+    // TODO: Use BookService instead of BookRepository after merge.
 
     @GetMapping("/")
     public String home(Model model) {
+        // TODO: Update to use BookService.getAllBooks() once available.
         model.addAttribute("books", bookRepository.findAll());
         return "index";
     }
