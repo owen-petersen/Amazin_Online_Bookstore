779cbc1e405fb2664eaad2864d063f9547a672b4
Faareh <faarehb@yahoo.com>
2025-11-01 20:15:14 -0400

    Feature: Add main page with HomeController and basic layout.

---
+++ src/main/java/net/azurewebsites/amazin_online_bookstore/HomeController.java
@@ -0,0 +1,19 @@
+package net.azurewebsites.amazin_online_bookstore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "index";
+    }
+}
