1e28c9cdcd7d1a27018d285071cda21104d353b3
Marvel <m4rvelados1000@gmail.com>
2025-11-02 20:32:54 -0500

    Feature: Create cart controller

---
+++ src/main/java/net/azurewebsites/amazin_online_bookstore/CartController.java
@@ -0,0 +1,10 @@
+package net.azurewebsites.amazin_online_bookstore;
+
+/**
+ * The CartController class
+ *
+ * @author Marvel Adotse-ogah
+ * @version 2025-11-02
+ */
+public class CartController {
+}
