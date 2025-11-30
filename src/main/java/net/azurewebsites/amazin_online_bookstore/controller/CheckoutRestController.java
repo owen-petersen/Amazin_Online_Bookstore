747adab979a7a4295d2b46c08647600902519e95
Owen Petersen <opetersen1010@hotmail.com>
2025-11-30 11:14:13 -0500

    Update jaccard distance when user makes a purchase

--- src/main/java/net/azurewebsites/amazin_online_bookstore/controller/CheckoutRestController.java
+++ src/main/java/net/azurewebsites/amazin_online_bookstore/controller/CheckoutRestController.java
@@ -1,11 +1,17 @@
 package net.azurewebsites.amazin_online_bookstore.controller;
 
+import jakarta.servlet.http.HttpSession;
 import net.azurewebsites.amazin_online_bookstore.datatransferobj.BookOrder;
+import net.azurewebsites.amazin_online_bookstore.entity.Person;
+import net.azurewebsites.amazin_online_bookstore.repository.PersonRepository;
 import net.azurewebsites.amazin_online_bookstore.service.BookService;
+import net.azurewebsites.amazin_online_bookstore.service.JaccardService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 /**
  * The CheckoutRestController class
@@ -16,20 +22,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/internal/checkout")
 public class CheckoutRestController {
+    PersonRepository personRepository;
+
     BookService bookService;
+    JaccardService jaccardService;
 
     public CheckoutRestController(BookService bookService) {
         this.bookService = bookService;
     }
 
     @PostMapping("/purchase")
-    public boolean purchase(@RequestBody BookOrder orderItems) {
+    public boolean purchase(HttpSession session, @RequestBody BookOrder orderItems) {
+        Optional<Person> person =
+                personRepository.findByUsername(String.valueOf(session.getAttribute("username")));
+
+        if (person.isEmpty()) { return false; }
+
         for (Integer item: orderItems.getItemIds()) {
 //            System.out.println("Book " + item + ": " + bookService.getById(item).getInventory());
             bookService.takeBookFromInventory(item);
 //            System.out.println("Book " + item + ": " + bookService.getById(item).getInventory());
         }
         // TODO MA-O Nov 15 25: Include the creation of a new purchase when Users are implemented.
+
+        jaccardService.UpdateJaccardsForUser(person.get());
+
         return true;
     }
 }
--- src/main/java/net/azurewebsites/amazin_online_bookstore/repository/PersonRepository.java
+++ src/main/java/net/azurewebsites/amazin_online_bookstore/repository/PersonRepository.java
@@ -3,7 +3,10 @@ package net.azurewebsites.amazin_online_bookstore.repository;
 import net.azurewebsites.amazin_online_bookstore.entity.Person;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface PersonRepository extends CrudRepository<Person,Integer> {
 
+    Optional<Person> findByUsername(String username);
     Person findByUsernameAndPassword(String username, String password);
 }
