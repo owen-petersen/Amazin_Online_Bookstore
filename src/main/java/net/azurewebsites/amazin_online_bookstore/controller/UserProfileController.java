d97925bdfc6f8760f90ab3642339831ac8218215
Marvel <m4rvelados1000@gmail.com>
2025-11-29 22:21:26 -0500

    Feature: User profile pages redirect to login if there is no existing session
    Feature: Implement purchase history page
    
    Feature: Add error code pages

--- src/main/java/net/azurewebsites/amazin_online_bookstore/controller/UserProfileController.java
+++ src/main/java/net/azurewebsites/amazin_online_bookstore/controller/UserProfileController.java
@@ -1,13 +1,18 @@
 package net.azurewebsites.amazin_online_bookstore.controller;
 
+import jakarta.servlet.http.HttpSession;
 import net.azurewebsites.amazin_online_bookstore.entity.Person;
+import net.azurewebsites.amazin_online_bookstore.entity.Purchase;
 import net.azurewebsites.amazin_online_bookstore.repository.PersonRepository;
+import net.azurewebsites.amazin_online_bookstore.repository.PurchaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
+
+import java.util.List;
 
 /**
  * The UserProfileController class manages request for information and pages unique to the user.
@@ -19,37 +24,60 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 @RequestMapping("/profile")
 public class UserProfileController {
     private PersonRepository personRepository;
+    private PurchaseRepository purchaseRepository;
 
     @Autowired
-    public UserProfileController(PersonRepository personRepository) {
+    public UserProfileController(PersonRepository personRepository,  PurchaseRepository purchaseRepository) {
         this.personRepository = personRepository;
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    private String getSessionUsername(HttpSession session) {
+        if (session.getAttribute("username") == null) {
+            return "redirect:/login";
+        }
+        return null;
     }
 
     @GetMapping("/")
-    public String defaultPage(@SessionAttribute(value = "username") String username, Model model) {
-        Person person = personRepository.findByUsername(username);
-        model.addAttribute("user", person); // To add the current user
+    public String defaultPage(HttpSession session, Model model) {
+        String x = getSessionUsername(session);
+        if (x != null) return x;
+        String username = (String) session.getAttribute("username");
+        Person user = personRepository.findByUsername(username);
+        model.addAttribute("user", user); // To add the current user
         return "profile/profile-page";
     }
 
     @GetMapping("/profile-page")
-    public String profilePage(@SessionAttribute(value = "username") String username, Model model) {
-        Person person = personRepository.findByUsername(username);
-        model.addAttribute("user", person);
+    public String profilePage(HttpSession session, Model model) {
+        String x = getSessionUsername(session);
+        if (x != null) return x;
+        String username = (String) session.getAttribute("username");
+        Person user = personRepository.findByUsername(username);
+        model.addAttribute("user", user);
         return "profile/profile-page";
     }
 
     @GetMapping("/purchase-history")
-    public String purchaseHistory(@SessionAttribute(value = "username") String username, Model model) {
-        Person person = personRepository.findByUsername(username);
-        model.addAttribute("user", person);
+    public String purchaseHistory(HttpSession session, Model model) {
+        String x = getSessionUsername(session);
+        if (x != null) return x;
+        String username = (String) session.getAttribute("username");
+        Person user = personRepository.findByUsername(username);
+        List<Purchase> purchases = purchaseRepository.findByBuyerOrderByDateTimeAsc(user);
+        model.addAttribute("user", user);
+        model.addAttribute("purchases", purchases);
         return "profile/purchase-history";
     }
 
     @GetMapping("/account-details")
-    public String accountDetails(@SessionAttribute(value = "username") String username, Model model) {
-        Person person = personRepository.findByUsername(username);
-        model.addAttribute("user", person);
+    public String accountDetails(HttpSession session, Model model) {
+        String x = getSessionUsername(session);
+        if (x != null) return x;
+        String username = (String) session.getAttribute("username");
+        Person user = personRepository.findByUsername(username);
+        model.addAttribute("user", user);
         return "profile/account-details";
     }
 }
