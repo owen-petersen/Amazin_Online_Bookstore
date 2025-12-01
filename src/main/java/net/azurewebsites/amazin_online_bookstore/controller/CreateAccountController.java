bb3cfa05d2ec7de6862787745f40ae4f1be455b1
Marvel <m4rvelados1000@gmail.com>
2025-12-01 10:10:59 -0500

    Feature: Complete create account page

--- src/main/java/net/azurewebsites/amazin_online_bookstore/controller/CreateAccountController.java
+++ src/main/java/net/azurewebsites/amazin_online_bookstore/controller/CreateAccountController.java
@@ -1,11 +1,14 @@
 package net.azurewebsites.amazin_online_bookstore.controller;
 
 import net.azurewebsites.amazin_online_bookstore.entity.Person;
+import net.azurewebsites.amazin_online_bookstore.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * The CreateAccountController class
@@ -16,8 +19,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/create-account")
 public class CreateAccountController {
+    private PersonService personService;
 
-    public CreateAccountController() {}
+    @Autowired
+    public CreateAccountController(PersonService personService) {
+        this.personService = personService;
+    }
 
     @GetMapping("")
     public String getCreateAccountPage() {
@@ -26,9 +33,18 @@ public class CreateAccountController {
 
     @PostMapping("")
     public String createAccount(@RequestParam("username") String username,
-                                @RequestParam("password") String password) {
+                                @RequestParam("password") String password,
+                                @RequestParam("firstname") String firstname,
+                                @RequestParam("lastname") String lastname,
+                                @RequestParam("email") String email,
+                                RedirectAttributes redirectAttributes) {
         // Use person service to check if username exists
         // If it doesn't, create and save a new user person account
-        return "redirect:/login";
+        if (personService.createAccount(username, password, firstname, lastname, email)) {
+            return "redirect:/login";
+        }
+
+        redirectAttributes.addFlashAttribute("error", "Account could not be created. Missing information");
+        return "redirect:/create-account";
     }
 }
